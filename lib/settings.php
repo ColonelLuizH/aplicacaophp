755965929a962dce0596e8e65af30e2dbcde3849
Luiz H <61873431+ColonelLuizH@users.noreply.github.com>
2021-12-14 06:44:02 -0300

    Commitando o projeto

---
+++ lib/settings.php
@@ -0,0 +1,24 @@
+<?php
+		#Dados do site
+		define("titulo_site", "Blog 2021");
+		define("url_site", "http://localhost/blog/"); //http://localhost/blog/
+
+
+		#Dados do banco
+		define("db_host", "localhost"); //localhost
+		define("db_usuario", "luizhaa"); //root
+		define("db_senha", ""); //$2y$10$IH1sOwf2aayxls0d3vl/zun391zEI88bkUJIM7cUlkyQnB0C0MzJe
+		define("db_banco", "blog1");
+
+		#Configurações de posts e limitadores
+		define("totalPostsPorPagina", 10); //Quantos posts serão mostrados por página
+		define("totalCaractersPosts", 366); //Máximo de caracteres no inicio
+
+
+		#INFORMAÇÕES DE CONTATO
+		define("contato_email", "colonelluizh@gmail.com");
+		define("contato_telefone", "(00) - 00000-0000");
+
+
+		
+?>
